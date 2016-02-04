2c10016e7d72dae38f836019f4c202f9fa32955e
Ryan Schlesinger <ryan@outstand.com>
2016-02-03 17:30:25 -0800

    Add bind interface, http address env vars

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,54 @@
 #!/usr/bin/dumb-init /bin/sh
 set -e
 
+debecho () {
+  if [ ! -z "$DEBUG" ]; then
+    echo "$1" >&2
+  fi
+}
+
 # Note above that we run dumb-init as PID 1 in order to reap zombie processes
 # as well as forward signals to all processes in its session. Normally, sh
 # wouldn't do either of these functions so we'd leak zombies as well as do
 # unclean termination of all our sub-processes.
 
+# We need to find the bind address if instructed to bind to an interface
+NOMAD_BIND=
+if [ -n "$NOMAD_BIND_INTERFACE" ]; then
+  debecho "NOMAD_BIND_INTERFACE=$NOMAD_BIND_INTERFACE"
+  debecho "$(ifconfig $NOMAD_BIND_INTERFACE)"
+  NOMAD_BIND_ADDRESS=$(ifconfig $NOMAD_BIND_INTERFACE | grep "inet addr:" | cut -d: -f2 | cut -d" " -f1)
+  if [ -z "$NOMAD_BIND_ADDRESS" ]; then
+    echo "Network interface $NOMAD_BIND_INTERFACE has no ip address, exiting."
+    exit 1
+  fi
+  debecho "NOMAD_BIND_ADDRESS=$NOMAD_BIND_ADDRESS"
+  NOMAD_BIND="-bind=$NOMAD_BIND_ADDRESS -network_interface=$NOMAD_BIND_INTERFACE"
+fi
+
+debecho "NOMAD_BIND=$NOMAD_BIND"
+
+NOMAD_DATA_DIR=/nomad/data
+NOMAD_CONFIG_DIR=/nomad/config
+
+if [ -n "$NOMAD_HTTP_ADDRESS" ]; then
+  debecho "NOMAD_HTTP_ADDRESS=$NOMAD_HTTP_ADDRESS"
+  cat > $NOMAD_CONFIG_DIR/local/http_address.hcl <<EOF
+addresses {
+  http = "$NOMAD_HTTP_ADDRESS"
+}
+EOF
+fi
+
 # Nomad's docker driver needs access to the docker socket.  We don't know what
 # the gid of the docker group in the host is so we figure it out here.
 if [ -S /var/run/docker.sock ]; then
-  usermod -a -G $(stat -c '%g' /var/run/docker.sock) nomad
+  GID=$(stat -c '%g' /var/run/docker.sock)
+  # Ensure the group exists
+  if ! getent group $GID; then
+    groupadd -g $GID docker
+  fi
+  usermod -a -G $GID nomad
 fi
 
 # This exposes three different modes, and allows for the execution of arbitrary
@@ -17,8 +56,6 @@ fi
 # the config directory, allowing for easy customization by placing JSON files
 # there. Note that there's a common config location, as well as one specifc to
 # the server and agent modes.
-NOMAD_DATA_DIR=/nomad/data
-NOMAD_CONFIG_DIR=/nomad/config
 if [ "$1" = 'dev' ]; then
     shift
     gosu nomad \
